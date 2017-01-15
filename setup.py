a6dce009474f07993c7d39ec7e4df3100ded4eb3
Dusan Klinec <dusan.klinec@gmail.com>
2017-01-15 17:44:16 +0100

    pip fixes - url fixed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.1.16'
+version = '0.1.17'
 
 # Please update tox.ini when modifying dependency version requirements
 install_requires = [
@@ -50,7 +50,7 @@ setup(
     name='ebclient.py',
     version=version,
     description='EnigmaBridge Python Client',
-    url='https://enigmabridge.com',
+    url='https://github.com/EnigmaBridge/client.py',
     author="Enigma Bridge",
     author_email='info@enigmabridge.com',
     license=open('LICENSE').read(),
